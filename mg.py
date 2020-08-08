39207468f2cb01d33ce1b43b965e343a2671c1aa
BlasterKing <69380948+BlasterKing@users.noreply.github.com>
2020-08-08 23:08:04 +0530

    Update mg.py

--- mg.py
+++ mg.py
@@ -1,10 +1,11 @@
 from urllib import request
-import os
+import time
 from requests import post,get
 num=input("Dialog number:")
 F=int(input("count:"))
 for i in range(F):
   url='https://megarun.dialog.lk/api/v2/user'
   body={'mobile':num[1:]}
-  os.sleep(5)
+  time.sleep(5)
   poreq=post(url,data=body)
+print("Sent")
